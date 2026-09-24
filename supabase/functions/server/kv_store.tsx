db92c5bc0fb25e90013f781b12aacff222655316
Your Name <you@example.com>
2026-09-25 01:02:14 +0530

    new supabase

--- supabase/functions/server/kv_store.tsx
+++ supabase/functions/server/kv_store.tsx
@@ -7,7 +7,7 @@ CREATE TABLE kv_store_3fde14d4 (
 );
 */
 
-// View at https://supabase.com/dashboard/project/wcmdmlqwuuicyhoafmki/database/tables
+// View at https://supabase.com/dashboard/project/tmrlpfjjczwvrahisslp/database/tables
 
 // This file provides a simple key-value interface for storing Figma Make data. It should be adequate for most small-scale use cases.
 import { createClient } from "jsr:@supabase/supabase-js@2.49.8";
